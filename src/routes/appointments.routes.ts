3ebad4466c8a360d043f67d892c22994e3b5260d
Breno Alves <breno550@hotmail.com>
2020-10-03 01:09:38 -0300

    fix

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,13 +1,22 @@
 import { Router } from 'express';
+import { uuid } from 'uuidv4';
 
 const appointmentsRouter = Router();
 
 const appointments = [];
 
 appointmentsRouter.post('/', (request, response) => {
-  const { provider,  } = request.body;
+  const { provider, date } = request.body;
 
-  return response.json({ message: 'hello world' });
+  const appointment = {
+    id: uuid(),
+    provider,
+    date,
+  };
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
